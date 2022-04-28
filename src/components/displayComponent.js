3532f427bdada2eb87850cb7d7e40e497e6d4b6c
Ol-create <oluola96@gmail.com>
2022-04-28 10:12:11 -0600

    feat: add display component

---
+++ src/components/displayComponent.js
@@ -0,0 +1,12 @@
+class DisplayComponent extends React.Component{
+	constructor(){
+		super();
+	}
+	render(){
+		return(
+			<div className="display"><p>0</p></div>
+		)
+	}
+}
+
+export default DisplayComponent;
