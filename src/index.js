996ecb670759ca52854e06b46317febefb6faf14
Ol-create <oluola96@gmail.com>
2022-04-30 02:19:13 -0600

    feat: render component to DOM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './components/calculator';
 import './App.css';
-import './index.css';
 
 // Render the Calculator to the Web page.
 ReactDOM.render(<Calculator />, document.getElementById('root'));
