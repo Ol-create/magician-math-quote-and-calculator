c4ace308fa2a0d61cce1b6ce981ce1a5be3eae3f
Ol-create <oluola96@gmail.com>
2022-04-30 13:38:05 -0600

    fix: resolve linters err

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,144 +1,32 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import OutputScreen from './outputScreen';
-import calculate from '../logic/calculate';
+import React from 'react';
 
-class Calculator extends React.Component {
+class Buttons extends React.Component {
   constructor(props) {
     super(props);
-    const { pre, operand, curr } = props;
-    this.state = {
-      total: pre,
-      operation: operand,
-      next: curr,
-    };
-    this.newChanges = this.newChanges.bind(this);
+    this.state = {};
+    this.btnInteract = this.btnInteract.bind(this);
   }
 
-  newChanges(bp) {
-    const x = calculate(this.state, bp);
-    this.setState(() => ({
-      total: x.total,
-      operation: x.operation,
-      next: x.next,
-    }));
+  btnInteract() {
+    const { btnInteract, label } = this.props;
+    btnInteract(label);
   }
 
   render() {
-    const { total, operation, next } = this.state;
+    const { btnName, label } = this.props;
     return (
-      <div className="container">
-        <OutputScreen
-          name="outputScreen"
-          label="0"
-          pre={total}
-          operand={operation}
-          curr={next}
-        />
-        <Buttons
-          btnName="zero"
-          label="0"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="one"
-          label="1"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="two"
-          label="2"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="three"
-          label="3"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="four"
-          label="4"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="five"
-          label="5"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="six"
-          label="6"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="seven"
-          label="7"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="eight"
-          label="8"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="nine"
-          label="9"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="multiply"
-          label="x"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="divide"
-          label={'\u00F7'}
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="add"
-          label="+"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="subtract"
-          label="-"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="equal"
-          label="="
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="decimal"
-          label="."
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="all-clear"
-          label="AC"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="positive-negative"
-          label="+/-"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-        <Buttons
-          btnName="percent"
-          label="%"
-          btnInteract={(s) => this.newChanges(s)}
-        />
-      </div>
+      <button type="button" id={btnName} onClick={this.btnInteract}>
+        {label}
+      </button>
     );
   }
 }
 
-export default Calculator;
+export default Buttons;
 
-Calculator.propTypes = {
-  pre: PropTypes.string.isRequired,
-  operand: PropTypes.string.isRequired,
-  curr: PropTypes.string.isRequired,
+Buttons.propTypes = {
+  btnName: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  btnInteract: PropTypes.func.isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './components/calculator';
 import './App.css';
-import './index.html'
+import './index.css';
 
 // Render the Calculator to the Web page.
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<Calculator pre="" operand="" curr="0" />, document.getElementById('root'));
