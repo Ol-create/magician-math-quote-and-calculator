7927cb455d61168dc8e1670bb4e9237d2a62fee8
Ol-create <oluola96@gmail.com>
2022-05-04 12:38:20 -0600

    feat: render App component to DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import Calculator from './components/calculator';
 import './App.css';
 import './index.css';
 
 // Render the Calculator to the Web page.
-ReactDOM.render(<Calculator pre="" operand="" curr="0" />, document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+	  <App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+  );
